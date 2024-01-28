a49f69ff11d8cca597771a4170c6aa96a523c057
DINUZEW <daciandinu99@gmail.com>
2024-01-28 22:26:50 +0200

    Update assets + packages (probably move to another laptop with a newer version of Angular)

--- main_app/src/app/components/carousel/carousel.component.ts
+++ main_app/src/app/components/carousel/carousel.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 import { Movies } from 'src/app/shared/movies.model';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, OnChanges {
   @Input() allMovies: Movies[];
   public genres: string[];
+  currentIndex = 0;
 
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.allMovies.length;
+  }
 
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.allMovies.length) % this.allMovies.length;
+  }
+
+  getCurrentImage(): string {
+    return this.allMovies[this.currentIndex]?.poster;
+  }
   constructor() {
   };
 
@@ -19,4 +32,10 @@ export class CarouselComponent implements OnInit {
     console.log(this.genres);
   }
 
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes?.['allMovies'] && changes?.['allMovies'].currentValue !== changes?.['allMovies'].previousValue) {
+      this.allMovies = changes['allMovies'].currentValue;
+    }
+  }
 }
